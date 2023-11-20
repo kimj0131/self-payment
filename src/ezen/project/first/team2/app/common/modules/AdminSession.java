566261527dfb648bd7814afe871a466a2a062c15
sigwanlee <bluecnt@hotmail.com>
2023-11-20 10:08:26 +0900

    관리자 로그인 클래스, 테스트 코드 작성

---
+++ src/ezen/project/first/team2/app/common/modules/AdminSession.java
@@ -0,0 +1,74 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// [SGLEE:20231120MON_094800] Created
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package ezen.project.first.team2.app.common.modules;
+
+public class AdminSession {
+	public enum LoginResult {
+		Ok, InvalidId, InvalidPassword
+	}
+
+	private static AdminSession mInstance = null;
+
+	private String mId = "";
+	private String mPassword = "";
+	private LoginResult mLoginResult = null;
+
+	// 생성자
+	private AdminSession() {
+	}
+
+	public static AdminSession getInstance() {
+		if (AdminSession.mInstance == null) {
+			AdminSession.mInstance = new AdminSession();
+			AdminSession.mInstance.loadInfo();
+		}
+
+		return AdminSession.mInstance;
+	}
+
+	// 로그인
+	public LoginResult login(String id, String password) {
+		if (this.mId.equals(id) && this.mPassword.equals(password))
+			this.mLoginResult = LoginResult.Ok;
+		else if (!this.mId.equals(id))
+			this.mLoginResult = LoginResult.InvalidId;
+		else if (!this.mPassword.equals(password))
+			this.mLoginResult = LoginResult.InvalidPassword;
+
+		return this.mLoginResult;
+	}
+
+	// 로그아웃
+	public void logout() {
+		this.mLoginResult = null;
+	}
+
+	// 로그인 결과 얻기
+	public LoginResult getLoginResult() {
+		return this.mLoginResult;
+	}
+
+	// 로그인 상태 확인
+	public boolean isLoginOk() {
+		return this.mLoginResult == LoginResult.Ok;
+	}
+
+	public void loadInfo() {
+		this.mId = "admin";
+		this.mPassword = "qwer1234!@#$";
+	}
+
+	public void saveInfo(String id, String password) {
+		if (!id.isEmpty()) {
+			this.mId = id;
+		}
+
+		if (!password.isEmpty()) {
+			this.mPassword = password;
+		}
+	}
+}
---
+++ src/ezen/project/first/team2/app/common/z_test/modules/TestAdminSession.java
@@ -0,0 +1,29 @@
+package ezen.project.first.team2.app.common.z_test.modules;
+
+import ezen.project.first.team2.app.common.modules.AdminSession;
+
+public class TestAdminSession {
+	public static void main(String[] args) {
+		// ID, PASSWD 모두 맞는 경우
+		final String ID = "admin";
+		final String PASSWD = "qwer1234!@#$";
+
+		// ID, PASSWD 모두 틀린 경우
+		// final String ID = "aa";
+		// final String PASSWD = "1234";
+
+		// PASSWD가 틀린 경우
+		// final String ID = "admin";
+		// final String PASSWD = "1234";
+
+		AdminSession as = AdminSession.getInstance();
+		AdminSession.LoginResult res = as.login(ID, PASSWD);
+		System.out.printf("login test: id:%s, pw:%s -> result:%s \n",
+				ID, PASSWD, res);
+
+		as.logout();
+		System.out.println("logout.");
+
+		System.out.printf("isLoginOk() -> %s \n", as.isLoginOk());
+	}
+}
