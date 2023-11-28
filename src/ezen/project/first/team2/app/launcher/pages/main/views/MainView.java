31a0f7cded5721fc9976452da42ec308ccac42ab
geunyoung gil <dhdy69@gmail.com>
2023-11-29 08:37:57 +0900

    gilgeunyoung

--- src/ezen/project/first/team2/app/launcher/pages/main/views/MainView.java
+++ src/ezen/project/first/team2/app/launcher/pages/main/views/MainView.java
@@ -6,7 +6,12 @@
 
 package ezen.project.first.team2.app.launcher.pages.main.views;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 
 import ezen.project.first.team2.app.common.framework.View;
 import ezen.project.first.team2.app.common.utils.UiUtils;
@@ -16,6 +21,12 @@ public class MainView extends View {
 	// -------------------------------------------------------------------------
 
 	private JButton mBtn0 = new JButton();
+	private JButton mBtn1 = new JButton();
+	private JButton mBtn2 = new JButton();
+	
+	JFrame frame = new JFrame("Box Layout");
+
+
 
 	// -------------------------------------------------------------------------
 
@@ -30,27 +41,71 @@ public class MainView extends View {
 	@Override
 	protected void onInit() {
 		super.onInit();
+	      this.setBackground(Color.PINK);
+
+	     
+
 	}
 
 	// 레이아웃 설정
 	@Override
 	protected void onSetLayout() {
+		 GridLayout g1 = new GridLayout(1, 3);
+	      
+	      g1.setHgap(30);
+	      g1.setVgap(30);
+	      
+	      setLayout(new GridLayout(1, 10, 50, 50));
+	      
+	      setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25));
+
 	}
 
 	// 컨트롤 추가
 	@Override
 	protected void onAddCtrls() {
-		this.mBtn0.setText("버튼0");
+		this.mBtn0.setText("Manage- ment app");
+		this.mBtn1.setText("New User app");
+		this.mBtn2.setText("Payment app");
 
-		this.add(this.mBtn0);
+
+		  add(mBtn0);
+	      add(mBtn1);
+	      add(mBtn2);
+
+
 	}
 
 	// 이벤트 리스너 추가
 	@Override
 	protected void onAddEventListeners() {
 		this.mBtn0.addActionListener(e -> {
-			UiUtils.showMsgBox("버튼0", MainPage.TITLE);
-		});
+		    UiUtils.showMsgBox("LOADING...", MainPage.TITLE);
+	      });
+
+	      mBtn1.addActionListener(e -> {
+	         
+	         UiUtils.showMsgBox("LOADING...", MainPage.TITLE);
+	      });
+
+	      mBtn2.addActionListener(e -> {
+	         
+	         UiUtils.showMsgBox("LOADING...", MainPage.TITLE);
+	      });
+	   
+	      this.mBtn0.addActionListener(e -> {
+	         UiUtils.showMsgBox("Login...", MainPage.TITLE);
+	         
+	      });
+	      this.mBtn1.addActionListener(e -> {
+	         UiUtils.showMsgBox("Login...", MainPage.TITLE);
+	         
+	      });
+	      this.mBtn2.addActionListener(e -> {
+	         UiUtils.showMsgBox("Login...", MainPage.TITLE);
+	         
+	      });
+
 	}
 
 	// 뷰가 표시될 때
