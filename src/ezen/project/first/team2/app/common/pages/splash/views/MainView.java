ffa1b7315e81255c61223026c49826e6d09130c7
ezen-sigwanlee <bluecnt@hotmail.com>
2023-12-02 15:49:50 +0900

    스플래시 페이지에 이미지 설정

--- src/ezen/project/first/team2/app/common/pages/splash/views/MainView.java
+++ src/ezen/project/first/team2/app/common/pages/splash/views/MainView.java
@@ -1,12 +1,15 @@
 package ezen.project.first.team2.app.common.pages.splash.views;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 
 import ezen.project.first.team2.app.common.framework.View;
 import ezen.project.first.team2.app.common.pages.splash.SplashPage;
 
 public class MainView extends View {
 	private JLabel mLabel0 = new JLabel();
+	private JLabel mImgLabel = new JLabel();
 
 	public MainView() {
 		super(SplashPage.VIEW_NUM_MAIN);
@@ -18,19 +21,29 @@ public class MainView extends View {
 
 	@Override
 	protected void onInit() {
+		var icon = new ImageIcon("resources/images/SplashImage copy.jpg");
+		this.mImgLabel.setIcon(icon);
 	}
 
 	@Override
 	protected void onSetLayout() {
+		setLayout(null);
 	}
 
 	@Override
 	protected void onAddCtrls() {
 		this.mLabel0.setText("스플래시 페이지");
 
-		System.out.println(this.getBounds());
+		final int LABEL_HEIGHT = 30;
 
-		this.add(this.mLabel0);
+		JLayeredPane p = new JLayeredPane();
+		p.setBounds(this.getBounds());
+		this.mLabel0.setBounds(10, this.getHeight() - LABEL_HEIGHT, this.getWidth(), LABEL_HEIGHT);
+		this.mImgLabel.setBounds(this.getBounds());
+		p.add(this.mImgLabel, JLayeredPane.DEFAULT_LAYER);
+		p.add(this.mLabel0, JLayeredPane.POPUP_LAYER);
+
+		this.add(p);
 	}
 
 	@Override
